2fb1a1c990788f7ee856fed9665b7b721ddf666d
Maarten Marx <maartenmarx@uva.nl>
2013-11-28 18:40:15 +0400

    draftlawvote extractor

--- DraftLaws/CreateDraftlawOverview.sh
+++ DraftLaws/CreateDraftlawOverview.sh
@@ -10,3 +10,18 @@ curl 'http://parliament.ge/index.php?option=com_content&view=article&id=2840%3Al
 sed 's/&nbsp;/ /g'    > DraftlawOverview.xml
 
 # Now call DraftlawOverview-Extractor.xquery and let it output to stdout and | sed '1d'
+
+
+# Part 2, create a list of voting records for each draft law
+
+# loop over all lines inthe previous spreadsheet
+
+# get the url from the previous spreadsheet and the draftlawID: using awk
+draftlawID=" 
+url="
+outfile=`echo $url   | sed 's%http://parliament.ge/files/Voting_Records/%%; s%[./]%-%g'`
+
+wget  -O "$outfile"  $url 
+unzip -p "$outfile" word/document.xml > "Soutfile.xml"
+
+# Now run  DraftlawVote-Extractor.xquery on "Soutfile.xml" and that gives the outcome of the vote plus the individual votes of all guys
